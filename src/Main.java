c11159448c6ed98ac15bbbf4f8c5e5107ffced7c
PioneerX7 <vita-kopy@yandex.ru>
2022-11-28 12:19:56 +0300

    задачи 4 - 5.

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,12 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
+        task5();
     }
 
     public static void task1() {
+
         System.out.println("Задача 1");
 
         var dog = 8;
@@ -16,28 +19,38 @@ public class Main {
         System.out.println("котов " + cat);
         System.out.println("бумаги " + paper);
     }
-        public static void task2() {
+    public static void task2() {
 
-            System.out.println("Задача 2");
+        System.out.println("Задача 2");
 
-            var dog = 8;
-            var cat = 3.6;
-            var paper = 763789;
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
 
-            dog = dog + 4;
-            cat = cat + 4;
-            paper = paper + 4;
+        System.out.println("собак " + dog);
+        System.out.println("котов " + cat);
+        System.out.println("бумаги " + paper);
 
-            System.out.println("собак " + dog);
-            System.out.println("котов " + cat);
-            System.out.println("бумаги " + paper);
-        }
-        public static void task3() {
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+
+        System.out.println("собак " + dog);
+        System.out.println("котов " + cat);
+        System.out.println("бумаги " + paper);
+    }
+
+    public static void task3() {
+
         System.out.println("Задача 3");
 
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
+
+        System.out.println("собак " + dog);
+        System.out.println("котов " + cat);
+        System.out.println("бумаги " + paper);
 
         dog = dog - 3.5;
         cat = cat - 1.6;
@@ -47,4 +60,33 @@ public class Main {
         System.out.println("котов " + cat);
         System.out.println("бумаги " + paper);
     }
+    public static void task4() {
+
+        System.out.println("Задача 4");
+
+        var friend = 19;
+
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+    }
+
+    public static void task5() {
+
+        System.out.println("Задача 5");
+
+        var frog = 3.5;
+
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+    }
+
+
 }
